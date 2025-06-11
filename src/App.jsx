6c6d8e1b08e4c441ed8737a1cd8011952ba8cc41
Noor Raj <rajmnoor@gmail.com>
2025-06-10 23:41:38 -0400

    fixed after merging

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import React from 'react';
 import Character from "./components/Character";
 import ironmanImage from "./images/ironman.png";
 import adamwarlockImage from "./images/adamwarlock.png";
